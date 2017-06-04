67a51089bd0879965185f4defba16123a725c731
Olga Sergeyeva <olgasergeyeva@outlook.com>
2017-06-04 12:06:32 +0500

    Добавлен класс Client

---
+++ EduCentre/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCentre
+{
+    class Client
+    {
+        public readonly int ClientID;
+        public string LastName;
+        public string FirstName;
+        public string SecondName;
+        public string PhoneNumber;
+        public string Email;
+    }
+}
