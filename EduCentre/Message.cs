cd78b36a21548264150538209ed4d23bb442ac6d
Olga Sergeyeva <olgasergeyeva@outlook.com>
2017-06-04 12:17:00 +0500

    Добавлен класс Message

---
+++ EduCentre/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCentre
+{
+    class Message
+    {
+        private int MessageId;
+        public int Sender;
+        public int Reciever;
+        public string Text;
+        public DateTime MessageDate;
+        public DateTime MessageTime;
+        public string Status;
+    }
+}
