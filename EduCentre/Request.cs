ff84b9d7e2f5a2436de1d253c4f30cdf1f40432e
Olga Sergeyeva <olgasergeyeva@outlook.com>
2017-06-04 12:23:57 +0500

    Добавлен класс Request

---
+++ EduCentre/Request.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCentre
+{
+    class Request
+    {
+        private int RequestId;
+        public int CourseId;
+        public string Text;
+        public DateTime MessageDate;
+        public DateTime MessageTime;
+        public string Status;
+    }
+}
