ad22909326be280fabb107c573f7674227d2844f
Olga Sergeyeva <olgasergeyeva@outlook.com>
2017-06-04 12:35:01 +0500

    Добавлены классы Course, Group, Class

---
+++ EduCentre/Class.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCentre
+{
+    class Class
+    {
+        public int ClassId;
+        public int CourseId;
+        public int GroupId;
+        public int TeacherId;
+        public string ClassName;
+        public DateTime ClassDate;
+        public DateTime ClassTime;
+        public int Classroom;
+    }
+}
---
+++ EduCentre/Course.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduCentre
+{
+    class Course
+    {
+        public int CourseId;
+        public string CourseName;
+        public string CourseType;
+        public int Duration;
+        public DateTime StartDate;
+        public DateTime EndDate;
+        public List<Teacher> Teachers;
+        public List<Group> Groups;
+        public List<Class> Classes;
+    }
+}
